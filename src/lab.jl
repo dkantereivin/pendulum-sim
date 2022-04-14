e65ae7575e0ddc4cdce91a870602cefdb6bed85c
David Kanter Eivin <kanter.dev@gmail.com>
2022-04-14 14:58:01 -0400

    add results to report

--- src/lab.jl
+++ src/lab.jl
@@ -1,12 +1,15 @@
 module lab
+import Base.@kwdef
 using Plots
 using DataFrames
 # Assume SI units unless otherwise specified!
 
-const g = 9.81
-const Δt = 1 / 1000
+export g, Δt, Bob, Pendulum, simulate, extremes, periods
 
-struct Bob
+const g = 9.81
+const Δt = 1 / 10000
+
+@kwdef struct Bob
     mass::Number
     diameter::Number
     γ::Number
@@ -20,9 +23,13 @@ struct Pendulum
     Pendulum(radius::Number, θ::Number, bob::Bob) = new((x = 0, y = 0), radius, θ, bob)
 end
 
-function simulate(pendulum::Pendulum, duration::Number)
+function simulate(pendulum::Pendulum, duration::Number; resistance=true)
     (; radius, θ, bob) = pendulum
     (; mass, diameter, γ) = bob
+
+    if resistance == false
+        γ = 0
+    end
 
     # change in acceleration in a vacuum
     ddθ(θ::Number) = -(sin(θ) * g) / radius
@@ -39,7 +46,7 @@ function simulate(pendulum::Pendulum, duration::Number)
     df
 end
 
-function extremes(df::AbstractDataFrame)
+function extremes(df::AbstractDataFrame)::AbstractDataFrame
     extremes = DataFrame(first(df))
     for i in 3:nrow(df)
         if sign(df[i-1, :ω]) ≠ sign(df[i, :ω])
@@ -49,11 +56,21 @@ function extremes(df::AbstractDataFrame)
     extremes
 end
 
+function periods(extremes::AbstractDataFrame)::Vector{Number}
+    acc = []
+    for i in range(1, nrow(extremes) - 2; step=2)
+        push!(acc, extremes[i + 2, :time] - extremes[i, :time])
+    end
+    acc
+end
+
 function main()
     b = Bob(0.01, .1, 0.25)
     p = Pendulum(1., deg2rad(50), b)
     df = simulate(p, 30)
-    e = extremes(df)
+    @time df = simulate(p, 30)
+    # e = extremes(df)
+    df
 end
 
 end
